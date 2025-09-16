0e144a0580ae9527a93ffce6ca8c0a635d463ad9
felix101930 <vaghs622@gmail.com>
2025-09-16 16:15:51 -0600

    fixes github link

--- app/week-2/student-info.js
+++ app/week-2/student-info.js
@@ -5,8 +5,8 @@ export default function StudentInfo() {
         <div>
             <h2>Name: Felix Montanez</h2>
             <p>
-                Github: <Link href="https://github.com/felix101930/cprg306-assignments" className="underline">
-                    https://github.com/felix101930/cprg306-assignments
+                Github: <Link href="https://github.com/felix101930/" className="underline">
+                    https://github.com/felix101930/
                 </Link>
             </p>
         </div>
