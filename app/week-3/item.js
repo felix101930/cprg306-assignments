e8eff3e568b9d9a2c44049f108bc234c610dcefc
felix101930 <vaghs622@gmail.com>
2025-09-23 14:26:44 -0600

    adds styling and puts it as list item

--- app/week-3/item.js
+++ app/week-3/item.js
@@ -1,8 +1,13 @@
 export default function Item({ name, quantity, category }) {
     return (
-        <div>
-            <h3>{name}</h3>
-            <p>Buy {quantity} in {category}</p>
-        </div>
+        <li className="bg-black p-2 my-2 rounded border border-gray-800">
+            <h3 className="text-xl">{name}</h3>
+            <p>
+                Quantity: {quantity}
+            </p>
+            <p>
+                Category: {category}
+            </p>
+        </li>
     );
 }
