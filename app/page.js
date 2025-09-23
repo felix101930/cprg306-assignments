f9b82f0411116b082029a280a916f17acca70a1f
felix101930 <vaghs622@gmail.com>
2025-09-23 14:37:11 -0600

    fixes paragraph for page.js

--- app/page.js
+++ app/page.js
@@ -7,14 +7,8 @@ export default function Home() {
         <h1 >CPRG 306: Web Development 2 - Assignments</h1>
         <div>
           <p>
-            <Link href="/week-2">
-              Go to Week 2
-            </Link>
-          </p>
-          <p>
-            <Link href="/week-3">
-              Go to Week 3
-            </Link>
+            <Link href="/week-2">Go to Week 2 → </Link>
+            <Link href="/week-3">Go to Week 3</Link>
           </p>
         </div>
       </main>
