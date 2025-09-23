fa8ea79422178bb752a2b79e8f41318e781fcb94
felix101930 <vaghs622@gmail.com>
2025-09-23 14:27:21 -0600

    adds styling

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from './item-list';
 
 export default function Page() {
     return (
-        <main>
-            <h1>Shopping List</h1>
+        <main className="p-4">
+            <h1 className="text-2xl mb-4">Shopping List</h1>
             <ItemList />
         </main>
     );
