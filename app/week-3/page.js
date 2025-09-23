acc53b695f4aa866263f48821253ca7d30b68338
felix101930 <vaghs622@gmail.com>
2025-09-23 14:32:18 -0600

    fixes styling for main and h1

--- app/week-3/page.js
+++ app/week-3/page.js
@@ -2,8 +2,8 @@ import ItemList from './item-list';
 
 export default function Page() {
     return (
-        <main className="p-4">
-            <h1 className="text-2xl mb-4">Shopping List</h1>
+        <main className="p-4 max-w-xl mx-auto">
+            <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
             <ItemList />
         </main>
     );
