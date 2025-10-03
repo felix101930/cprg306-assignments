ed107f3cd299de7473556c90f6df42ab547320f1
felix101930 <vaghs622@gmail.com>
2025-10-02 20:54:38 -0600

    adds week4

---
+++ app/week-4/page.js
@@ -0,0 +1,10 @@
+import NewItem from './new-item';
+
+export default function Page() {
+    return (
+        <main className="p-2">
+            <h1 className="text-3xl font-bold mb-4">New Item</h1>
+            <NewItem />
+        </main>
+    );
+}
