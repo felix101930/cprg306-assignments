c2dd4f7d9de0683e670895680a175b9c58b3a2bc
felix101930 <vaghs622@gmail.com>
2025-10-16 10:18:31 -0600

    fixes case

--- app/week-5/new-item.js
+++ app/week-5/new-item.js
@@ -59,17 +59,17 @@ export default function NewItem() {
                     required
                     className="w-full p-2 text-lg bg-gray-800 text-white rounded-lg focus:outline-none focus:ring-2 focus:ring-blue-500"
                 >
-                    <option value="produce">Produce</option>
-                    <option value="dairy">Dairy</option>
-                    <option value="bakery">Bakery</option>
-                    <option value="meat">Meat</option>
-                    <option value="frozen">Frozen Foods</option>
-                    <option value="canned">Canned Goods</option>
-                    <option value="dry">Dry Goods</option>
-                    <option value="beverages">Beverages</option>
-                    <option value="snacks">Snacks</option>
-                    <option value="household">Household</option>
-                    <option value="other">Other</option>
+                    <option value="Produce">Produce</option>
+                    <option value="Dairy">Dairy</option>
+                    <option value="Bakery">Bakery</option>
+                    <option value="Meat">Meat</option>
+                    <option value="Frozen">Frozen Foods</option>
+                    <option value="Canned">Canned Goods</option>
+                    <option value="Dry">Dry Goods</option>
+                    <option value="Beverages">Beverages</option>
+                    <option value="Snacks">Snacks</option>
+                    <option value="Household">Household</option>
+                    <option value="Other">Other</option>
                 </select>
             </div>
 
