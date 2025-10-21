f75546a334b1d9d16a6465b1edee11b6193e7825
felix101930 <vaghs622@gmail.com>
2025-10-21 15:28:24 -0600

    imports new item and item list

--- app/week-7/page.js
+++ app/week-7/page.js
@@ -1,9 +1,16 @@
+'use client';
+
+import { useState } from 'react';
 import ItemList from './item-list';
+import NewItem from './new-item';
+
+
 
 export default function Page() {
     return (
         <main className="p-4 max-w-xl mx-auto">
             <h1 className="text-2xl font-bold mb-3">Shopping List</h1>
+            <NewItem />
             <ItemList />
         </main>
     );
