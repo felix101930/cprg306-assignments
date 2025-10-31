02e014cd1eeeee32ec337560f3af283552379a62
felix101930 <vaghs622@gmail.com>
2025-10-31 16:51:45 -0600

    modify item js

--- app/week-8/item.js
+++ app/week-8/item.js
@@ -1,6 +1,9 @@
-export default function Item({ name, quantity, category }) {
+export default function Item({ name, quantity, category, onSelect }) {
     return (
-        <li className="bg-black p-2 my-2 rounded border border-gray-800">
+        <li 
+            className="bg-black p-2 my-2 rounded border border-gray-800 hover:bg-gray-900 cursor-pointer transition-colors"
+            onClick={() => onSelect(name)}
+        >
             <h3 className="text-xl">{name}</h3>
             <p>
                 Quantity: {quantity}
